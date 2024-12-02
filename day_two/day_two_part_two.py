58a47b4e66e799a766d94f8df90ec391974e336a
Charl <105985808+CharlRae@users.noreply.github.com>
2024-12-02 17:54:00 +0000

    Update day_two_part_two.py
    Added some commands to make the code slightly barable to read

--- day_two/day_two_part_two.py
+++ day_two/day_two_part_two.py
@@ -5,7 +5,7 @@ def report_is_safe(report_list):
     if (report_list[0] - report_list[1] < -3) or (report_list[0] - report_list[1] >= 0):
         return False
     
-    return report_is_safe(report_list[1:])
+    return report_is_safe(report_list[1:]) # Calls itself with the list excluding the first element
 
 
 
@@ -15,21 +15,22 @@ total = 0
 with open(file_name, 'r') as f:
     for report in f:
         report_list = report.split()
-        report_list = list(map(int, report_list))
-        if report_list[0] - report_list[1] > 0:
+        report_list = list(map(int, report_list)) # Converts each item in the list to int
+
+        if report_list[0] - report_list[1] > 0: # Forces list to be increasing
             report_list = report_list[::-1]
         if report_is_safe(report_list):
             total += 1
         else:
-            works = False
+            safe_report = False
+            
             for i in range(len(report_list)):
-
-                new_list = report_list[:i] + report_list[i+1:]
-                if new_list[0] - new_list[1] > 0:
+                new_list = report_list[:i] + report_list[i+1:] # Creates a new list excluding element i
+                if new_list[0] - new_list[1] > 0: # Forces new list to be increasing
                     new_list = new_list[::-1]
                 if report_is_safe(new_list):
-                    works = True
-            if works:
+                    safe_report = True
+            if safe_report:
                 total += 1
 
 print(total)
