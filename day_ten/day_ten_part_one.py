ac9a7784e816598f391f5c8558f90e743e11e74e
Charl <105985808+CharlRae@users.noreply.github.com>
2024-12-12 03:09:28 +0000

    Completed day 10
    Completed late since I for some reason was struggling to find a way to store the trails, but decided to come back at it with a fresh mind after a while and solves easily.

---
+++ day_ten/day_ten_part_one.py
@@ -0,0 +1,46 @@
+# Creates map_list, a 2D array representing the map
+map_list = []
+with open('input.txt', 'r') as f:
+    for line in f:
+        line = line.strip()
+        line = list(map(int, list(line))) # Converts line to a list of integers
+        map_list.append(line)
+
+# Finds trails from a start coordinate of (x, y)
+def find_trails(this_trail, height=0):
+    if height == 9:
+        trail_list.append(this_trail)
+        return None
+    # Check above
+    if this_trail[-1][1] - 1 >= 0 and map_list[this_trail[-1][1] - 1][this_trail[-1][0]] == height + 1:
+        find_trails(this_trail + [(this_trail[-1][0], this_trail[-1][1] - 1)], height + 1)
+    # Check below
+    if this_trail[-1][1] + 1 < len(map_list) and map_list[this_trail[-1][1] + 1][this_trail[-1][0]] == height + 1:
+        find_trails(this_trail + [(this_trail[-1][0], this_trail[-1][1] + 1)], height + 1)
+    # Check left
+    if this_trail[-1][0] - 1 >= 0 and map_list[this_trail[-1][1]][this_trail[-1][0] - 1] == height + 1:
+        find_trails(this_trail + [(this_trail[-1][0] - 1, this_trail[-1][1])], height + 1)
+    # Check right
+    if this_trail[-1][0] + 1 < len(map_list) and map_list[this_trail[-1][1]][this_trail[-1][0] + 1] == height + 1:
+        find_trails(this_trail + [(this_trail[-1][0] + 1, this_trail[-1][1])], height + 1)
+
+
+
+total = 0
+
+for y in range(len(map_list)):
+    for x in range(len(map_list[y])):
+        coordinate = (x, y)
+        if map_list[coordinate[1]][coordinate[0]] == 0: # If coordinate is a 0
+            # Resets variables
+            trail_list = []
+            seen_endpoints = set()
+            score = 0
+            find_trails([coordinate])
+            for trail in trail_list:
+                if trail[-1] not in seen_endpoints: # Counts trails ending at unique endpoints
+                    score += 1
+                    seen_endpoints.add(trail[-1])
+            total += score
+            
+print(total)
